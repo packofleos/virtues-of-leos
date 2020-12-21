cb2c2c9e95006326917dd8bdf7d5828f8f637b00
euphonicazure <38431726+euphonicazure@users.noreply.github.com>
2020-12-21 15:15:20 +0600

    added css classes in task form widget

--- virtues/forms.py
+++ virtues/forms.py
@@ -13,18 +13,19 @@ class TaskHistoryForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        # add css classes
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': f'{name}-input'})
         # we don't want duplicate tasks
         self.fields['task'].disabled = True
 
     def clean(self):
         cleaned_data = super().clean()
-
         task = cleaned_data.get('task')
         if task:
             if cleaned_data.get('amount') > task.max_amount:
                 # cannot exceed the task's max_amount
                 raise ValidationError(f"Amount cannot be greater than {task}'s max_amount ({task.max_amount}).")
-
         date = cleaned_data.get('date') 
         if date:
             if  date > timezone.now().date():
