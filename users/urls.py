4bb1b4319ec32969a763c563029b0feaab5c3f5d
euphonicazure <38431726+euphonicazure@users.noreply.github.com>
2020-12-26 12:31:22 +0600

    added logout feature

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
+from django.contrib.auth import views
 
 app_name = 'users'
 urlpatterns = [
-    # Include default auth urls.
-    path('', include('django.contrib.auth.urls')),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('logout/', views.LogoutView.as_view(), name='logout')
 ]
