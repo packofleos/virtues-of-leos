dc3b767ab6c8ee4683b42bae5f4f3eb9a0cd0257
Euphyzr <38431726+Euphyzr@users.noreply.github.com>
2021-03-23 22:59:57 +0600

    fix: KeyError for _set_highest when some task required by the title wasn't submitted

--- virtues/ext/title/__init__.py
+++ virtues/ext/title/__init__.py
@@ -15,8 +15,15 @@ def _set_highest(title, per_task, **kwargs):
     """Verify users' qualification for Highest score based titles."""
     top_users = set()
     for task in title.tasks.all():
-        # reversed sorted dictionary is expected
-        top_users.add(list(per_task[str(task)])[0])
+        try:
+            # reversed sorted dictionary is expected so top user for
+            # per_task['task name'] is the the first user i.e list(...)[0] 
+            top_users.add(list(per_task[str(task)])[0])
+        except KeyError:
+            # no user submitted a specfic task required by the title
+            # So, no one gets gets the title
+            top_users.clear()
+            break
 
     title.users.remove(title.users.all().first())
     if len(top_users) == 1:
