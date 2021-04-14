9279bb03d3ff1cf951a6c21b05318418a83b3490
Euphyzr <38431726+Euphyzr@users.noreply.github.com>
2021-04-14 23:08:55 +0600

    fix: config being str instead of int

--- virtues/app_conf.py
+++ virtues/app_conf.py
@@ -1,4 +1,4 @@
 import os
 
-MAX_DAY = os.environ.get('VIRTUES_RESET_DAYS', 7)
-MAX_AMOUNT = os.environ.get('VIRTUES_MAX_AMOUNT', 1000)
+MAX_DAY = int(os.environ.get('VIRTUES_RESET_DAYS', 7))
+MAX_AMOUNT = int(os.environ.get('VIRTUES_MAX_AMOUNT', 1000))
